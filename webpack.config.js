053c6e0010966f1a6fdc3dccf8ab73003ad3748f
ahmad-karimigorji <ahmadkarimigorji@yahoo.com>
2023-07-31 17:16:11 +0330

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: "./src/ts/app.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+          mangle: true,
+        },
+      }),
+    ],
+  },
+};
